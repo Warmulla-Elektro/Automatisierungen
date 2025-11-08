d6d2d8b092eedf58d746dea13089276c01dcfb8f
burdoto <t_burdow@warmulla-elektro.d>
2025-11-08 15:59:29 +0100

    suspend

--- hourtables/debug.sh
+++ hourtables/debug.sh
@@ -2,4 +2,4 @@
 
 ./pull.sh
 ./gradlew simplifyShadow || (echo 'Project build failed' && exit 1)
-(cd build/libs && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar hourtables.jar -op)
+(cd build/libs && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar hourtables.jar -op)
