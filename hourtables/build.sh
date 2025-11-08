4de56b65f54a920f650c88d43e2be4d663df3d5e
burdo <contact@kaleidox.de>
2025-11-08 16:35:46 +0100

    create latest.log

--- hourtables/build.sh
+++ hourtables/build.sh
@@ -3,4 +3,4 @@
 ./pull.sh
 rm -rf build/
 ./gradlew installDist || (echo 'Project build failed' && exit 1)
-./build/install/hourtables/bin/hourtables -up
+./build/install/hourtables/bin/hourtables -up > latest.log
