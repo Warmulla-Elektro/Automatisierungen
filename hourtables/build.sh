e0450c3afea3418797d52fbb3060da95ee7f1af1
burdoto <t_burdow@warmulla-elektro.d>
2025-11-08 15:33:06 +0100

    handle error cases

--- hourtables/build.sh
+++ hourtables/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./pull.sh
-./gradlew installDist
-cd build/install/hourtables/bin
-./hourtables -up
+rm -rf build/
+./gradlew installDist || (echo 'Project build failed' && exit 1)
+(cd build/install/hourtables/bin && ./hourtables -up)
