72d88ee178da3b3ef038619f7708a8aefd26aa39
burdo <contact@kaleidox.de>
2025-11-08 16:09:51 +0100

    run in correct working dir

--- hourtables/debug.sh
+++ hourtables/debug.sh
@@ -2,4 +2,4 @@
 
 ./pull.sh
 ./gradlew simplifyShadow || (echo 'Project build failed' && exit 1)
-(cd build/libs && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar hourtables.jar -op)
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar build/libs/hourtables.jar -op
