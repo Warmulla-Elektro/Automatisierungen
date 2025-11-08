0964630071e505cda0a924caab0ff01f1f1f675d
burdo <contact@kaleidox.de>
2025-11-08 15:56:57 +0100

    debug adjustments

--- hourtables/debug.sh
+++ hourtables/debug.sh
@@ -2,4 +2,4 @@
 
 ./pull.sh
 ./gradlew simplifyShadow || (echo 'Project build failed' && exit 1)
-(cd build/libs && java -agentlib:jdwp=transport=dt_socket,server=n,address=*:5005,suspend=y -jar hourtables.jar -up)
+(cd build/libs && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar hourtables.jar -op)
